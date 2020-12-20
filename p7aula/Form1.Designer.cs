3ec1f9e7ffc8a4b732f15d91bcb7b359a7fac150
macmiller87 <macamagolf@gmail.com>
2020-12-20 17:47:45 -0300

    Adicionar arquivos de projeto.

---
+++ p7aula/Form1.Designer.cs
@@ -0,0 +1,154 @@
+﻿namespace p7aula
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtSenha = new System.Windows.Forms.TextBox();
+            this.btnAcessar = new System.Windows.Forms.Button();
+            this.btbLimpar = new System.Windows.Forms.Button();
+            this.btnSair = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblRes = new System.Windows.Forms.Label();
+            this.lblNome = new System.Windows.Forms.Label();
+            this.txtNome = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // txtSenha
+            // 
+            this.txtSenha.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.txtSenha.Location = new System.Drawing.Point(178, 72);
+            this.txtSenha.Name = "txtSenha";
+            this.txtSenha.Size = new System.Drawing.Size(361, 26);
+            this.txtSenha.TabIndex = 1;
+            // 
+            // btnAcessar
+            // 
+            this.btnAcessar.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btnAcessar.Location = new System.Drawing.Point(347, 128);
+            this.btnAcessar.Name = "btnAcessar";
+            this.btnAcessar.Size = new System.Drawing.Size(108, 38);
+            this.btnAcessar.TabIndex = 3;
+            this.btnAcessar.Text = "Acessar";
+            this.btnAcessar.UseVisualStyleBackColor = true;
+            this.btnAcessar.Click += new System.EventHandler(this.btnAcessar_Click);
+            // 
+            // btbLimpar
+            // 
+            this.btbLimpar.BackColor = System.Drawing.SystemColors.Highlight;
+            this.btbLimpar.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btbLimpar.Location = new System.Drawing.Point(431, 251);
+            this.btbLimpar.Name = "btbLimpar";
+            this.btbLimpar.Size = new System.Drawing.Size(108, 38);
+            this.btbLimpar.TabIndex = 4;
+            this.btbLimpar.Text = "Limpar";
+            this.btbLimpar.UseVisualStyleBackColor = false;
+            this.btbLimpar.Click += new System.EventHandler(this.btbLimpar_Click);
+            // 
+            // btnSair
+            // 
+            this.btnSair.BackColor = System.Drawing.Color.Red;
+            this.btnSair.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btnSair.Location = new System.Drawing.Point(266, 251);
+            this.btnSair.Name = "btnSair";
+            this.btnSair.Size = new System.Drawing.Size(108, 37);
+            this.btnSair.TabIndex = 5;
+            this.btnSair.Text = "Sair";
+            this.btnSair.UseVisualStyleBackColor = false;
+            this.btnSair.Click += new System.EventHandler(this.btnSair_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(12, 73);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(160, 19);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Informe Sua Senha:";
+            // 
+            // lblRes
+            // 
+            this.lblRes.AutoSize = true;
+            this.lblRes.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lblRes.Location = new System.Drawing.Point(12, 190);
+            this.lblRes.Name = "lblRes";
+            this.lblRes.Size = new System.Drawing.Size(0, 19);
+            this.lblRes.TabIndex = 7;
+            // 
+            // lblNome
+            // 
+            this.lblNome.AutoSize = true;
+            this.lblNome.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lblNome.Location = new System.Drawing.Point(13, 34);
+            this.lblNome.Name = "lblNome";
+            this.lblNome.Size = new System.Drawing.Size(156, 19);
+            this.lblNome.TabIndex = 8;
+            this.lblNome.Text = "Informe Seu Nome:";
+            // 
+            // txtNome
+            // 
+            this.txtNome.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.txtNome.Location = new System.Drawing.Point(178, 34);
+            this.txtNome.Name = "txtNome";
+            this.txtNome.Size = new System.Drawing.Size(361, 26);
+            this.txtNome.TabIndex = 9;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(565, 296);
+            this.Controls.Add(this.txtNome);
+            this.Controls.Add(this.lblNome);
+            this.Controls.Add(this.lblRes);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnSair);
+            this.Controls.Add(this.btbLimpar);
+            this.Controls.Add(this.btnAcessar);
+            this.Controls.Add(this.txtSenha);
+            this.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.Name = "Form1";
+            this.Text = "VERIFICAÇÃO DE SENHA E ACESSO AO SISTEMA";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtSenha;
+        private System.Windows.Forms.Button btnAcessar;
+        private System.Windows.Forms.Button btbLimpar;
+        private System.Windows.Forms.Button btnSair;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblRes;
+        private System.Windows.Forms.Label lblNome;
+        private System.Windows.Forms.TextBox txtNome;
+    }
+}
+
---
+++ p7aula/Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace p7aula
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAcessar_Click(object sender, EventArgs e)
+        {
+            string AcesPerSenha, AcesPerNome, lblNome, lblSenha;
+
+            AcesPerSenha = "PROGRAMAÇÃO C#";
+            AcesPerNome = "MACMILLER";
+            lblNome = txtNome.Text;
+            lblSenha = txtSenha.Text;
+
+            if (string.Equals(lblSenha,AcesPerSenha) && string.Equals(lblNome,AcesPerNome)) {
+
+                lblRes.Text = ("Acesso liberado Com Sucesso!");
+            } else {
+                lblRes.Text = ("Erro Senha Não Compativel, Acesso Negado!");
+            }
+        }
+
+        private void btbLimpar_Click(object sender, EventArgs e)
+        {
+            txtSenha.Text = " ";
+            lblRes.Text = " ";
+            txtNome.Text = " ";
+        }
+
+        private void btnSair_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
